d22dabaa50f10e7b4cf5621c1b645bf40f40ce33
Gavin Grenda <gavin@pop-os.localdomain>
2020-05-19 23:09:44 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react'
+import AppDisplay from './AppDisplay'
 import './App.css';
+import FishContextProvider from './FishContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    // fishCount: 0,
+    // fps: 0,
+    // fpc: 1,
+    fishPoleImg: require('./images/fishingrod.png')
+  }
+
+  // handleClick = () => {
+  //   this.setState( prevState => {
+  //     return {fishCount: prevState.fishCount + prevState.fpc}
+  //   })
+  // }
+
+  render()  {
+      return (
+        <div>
+          <FishContextProvider>
+            <AppDisplay state={this.state} handleClick={this.handleClick}/>
+          </FishContextProvider>
+        </div>
+      )
+  }
 }
 
 export default App;
